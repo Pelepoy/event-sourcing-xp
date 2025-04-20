5e480fbb850c4256178fbf4345f6381b423d4739
Pelepoy <philipmarcel.maratas@gmail.com>
2025-04-20 13:03:40 +0800

    fix: correct table name in user experience migration

--- database/migrations/2025_04_20_040553_create_user_experience_table.php
+++ database/migrations/2025_04_20_040553_create_user_experience_table.php
@@ -4,14 +4,13 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('user_experience', function (Blueprint $table) {
+        Schema::create('user_experiences', function (Blueprint $table) {
             $table->id();
             $table->string('email');
             $table->unsignedBigInteger('user_id')->nullable();
